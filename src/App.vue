ee24f12da8fb3cd1d040e7255218987a30d0122e
Frantisek Farkas <frantisekfarkas93@gmail.com>
2017-10-29 23:45:26 +0100

    added homepage component, main.css and minor changes

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,15 @@
 </template>
 
 <script>
-  import HomePage from './components/HomePage'
-
   export default {
-    name: 'app',
-    components: {HomePage}
+    name: 'app'
+
   }
 </script>
 
 <style scoped lang="scss">
 
-  @import "./assets/scss/main";
+  @import "assets/scss/main";
 
-  #app {
-    font-family: Helvetica Neu, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+
 </style>
